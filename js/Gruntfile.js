de392bfb996f1593faaaf1f2e23c53f6ea3a2f5a
piotrgondek <piotrgondek@gmail.com>
2015-05-28 23:10:12 +0200

    grunt config

--- js/Gruntfile.js
+++ js/Gruntfile.js
@@ -1,45 +1,9 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-        concat: {
-            options: {
-                separator: ';'
-            },
-            basic: {
-                src: [
-                    'lib/jquery.js',
-                    'lib/lodash.js',
-                    'lib/backbone.js',
-                    'lib/backbone.marionette.js'
-                ],
-                dest: 'basic.js'
-            },
-            app: {
-                src: [
-                    'application/backbone.namespaces.js',
-                    'application/RootView.js',
-                    'application/routers/*.js',
-                    'application/app.js'
-                ],
-                dest: 'app.js'
-            },
-            components: {
-                src: [
-                    'application/components/gallery/*.js',
-                    'application/components/product/*.js'
-                ],
-                dest: 'components.js'
-            }
-        },
         uglify: {
             options: {
-                mangle: {
-                    except: [
-                        'jQuery',
-                        'Backbone',
-                        'Marionette'
-                    ]
-                }
+                sourceMap: true
             },
             basic: {
                 files: {
@@ -50,27 +14,45 @@ module.exports = function (grunt) {
                         'lib/backbone.marionette.js'
                     ]
                 }
+            },
+            app: {
+                files: {
+                    'app.min.js': [
+                        [
+                            'application/backbone.namespaces.js',
+                            'application/RootView.js',
+                            'application/routers/*.js',
+                            'application/app.js'
+                        ]
+                    ]
+                }
+            },
+            components: {
+                files: {
+                    'components.min.js': [
+                        'application/components/gallery/*.js',
+                        'application/components/product/*.js'
+                    ]
+                }
             }
         },
         watch: {
             basic: {
                 files: ['lib/*.js'],
-                tasks: ['uglify']
+                tasks: ['uglify:basic']
             },
             app: {
                 files: ['application/**/*.js'],
-                tasks: ['concat']
+                tasks: ['uglify:app', 'uglify:components']
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', [
         'uglify',
-        'concat',
         'watch'
     ]);
 };
